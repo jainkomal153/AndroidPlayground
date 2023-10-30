ff9cb041b82a9616cb2c90cb97a6a9d7839970f6
jainkomal153 <jainkomal153@gmail.com>
2023-10-30 12:53:18 +0530

    fixed some minor issues while building the project

--- SignUpApp/app/src/main/java/com/example/signupapp/DisplayActivity.kt
+++ SignUpApp/app/src/main/java/com/example/signupapp/DisplayActivity.kt
@@ -9,17 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class DisplayActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display)
+        setContentView(com.example.signupapp.R.layout.activity_display)
 
         val map = DataSource().loadDb()
         map.keys.forEach {
             key-> map.put(key, intent?.extras?.getString(key).toString())
         }
         val adapter = DisplayAdapter(this, map)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val recyclerView = findViewById<RecyclerView>(com.example.signupapp.R.id.recycler_view)
         recyclerView.adapter = adapter
 
 
